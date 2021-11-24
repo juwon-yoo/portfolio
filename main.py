ccec08724945b0df7004b80f4725523d341f7ea8
juwon <busker66@daum.net>
2021-11-25 01:22:27 +0900

    db_connect

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
-from flask import Flask, render_template, redirect, request, url_for
-from flask_restx import Api, Resource
+from flask import Flask, render_template, request, url_for
+from flask_restx import Api, Resource, reqparse
+from sqlalchemy import create_engine
+from sqlalchemy import text
 
 app = Flask(__name__)
 api = Api(app)
+
+# DB 연결
+test_config=None
+
+if test_config is None:      
+    app.config.from_pyfile("config.py")
+else:
+    app.config.update(test_config)
+
+database = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)   
+app.database = database
+
+parser = reqparse.RequestParser()
+parser.add_argument('id', help='프로젝트ID', type=int, required=True)
 
 @app.route('/main')
 def main():
     return render_template('main.html')
 
-@app.route('/contact', methods=['GET'])
+@app.route('/detail/<id>', methods=['GET'])
 def contact():
     if request.method=='GET':
-        return render_template('contact.html')
+
+        sql = 'SELECT * FROM project'
+        query = {
+            'id': id
+        }      
+        rows = app.database.execute(text(sql)).fetchall()
+        retVal = []
+        for row in rows:
+            r = {  
+                    'p_name'     : row['p_name'],
+                    'team'   : row['team'],
+                    'stack'     : row['stack'],
+                    'd'     : row['detail'],
+                    'id'   : row['id']                                 
+                }
+            retVal.append(r)
+        return render_template('detail.html', label=retVal)
+
+
 
 if __name__ == "__main__":
     #서버 시작
