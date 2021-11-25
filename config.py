95866a2383b7e8f9ab704a3cd646743022e7ad65
juwon <busker66@daum.net>
2021-11-26 04:03:11 +0900

    done

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 db = {
     'user'     : 'newuser',		
-    'password' : 'root',		
+    'password' : 'root1212',		
     'host'     : '127.0.0.1',	
     'port'     : 3306,			
     'database' : 'portfolio'		
